30dbc3520aea90dae7230027efb12ae77add5766
Ignacio Rodríguez <ignacio@sugarlabs.org>
2016-02-06 21:31:06 -0300

    😀

--- js/samplesviewer.js
+++ js/samplesviewer.js
@@ -1,4 +1,5 @@
 // Copyright (C) 2015 Sam Parkinson
+// Copyright (C) 2016 Ignacio Rodríguez
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the The GNU Affero General Public
 // License as published by the Free Software Foundation; either
